5e2304c044db035a976ed32aaa7fa0b6a81905d9
Romain Loeuillet <loeuillet.romain@gmail.com>
2018-08-14 15:30:16 +0200

    done 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
 
-  get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks", to: "tasks#index"
+  # get "tasks/:id", to: "tasks#show", as: :task
 
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
 
-  delete '/tasks/:id', to: "tasks#destroy", as: :delete_task
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # delete '/tasks/:id', to: "tasks#destroy", as: :delete_task
+
+  resources :tasks
 end
